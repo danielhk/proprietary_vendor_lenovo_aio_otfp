0e11cfdcc70b2093bcb8a442df70980045f6746c
danielhk <daniel_hk>
2017-10-19 10:14:13 +0800

    Replace the find-copy-subdir-files script
    The find-copy-subdir-files defined in build will copy everything
    including the backup files. Replace it with my own script which will
    ignore the backup files.

--- aio_otfp-vendor-blobs.mk
+++ aio_otfp-vendor-blobs.mk
@@ -1,13 +1,17 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/proprietary/bin,system/bin)
+define find-copy-files
+$(sort $(shell find $(2) -name "$(1)" -type f | grep -v "~" | $(SED_EXTENDED) "s:($(2)/?(.*)):\\1\\:$(3)/\\2:" | sed "s://:/:g"))
+endef
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-files,*,vendor/lenovo/aio_otfp/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-files,*,vendor/lenovo/aio_otfp/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/proprietary/lib64,system/lib64)
+PRODUCT_COPY_FILES += $(call find-copy-files,*,vendor/lenovo/aio_otfp/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/proprietary/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-files,*,vendor/lenovo/aio_otfp/proprietary/lib64,system/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/proprietary/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-files,*,vendor/lenovo/aio_otfp/proprietary/usr,system/usr)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/aio_otfp/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-files,*,vendor/lenovo/aio_otfp/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-files,*,vendor/lenovo/aio_otfp/proprietary/vendor,system/vendor)
